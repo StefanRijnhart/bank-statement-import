a0d12d6aaa3e6d78e0c917f0d465d33e314479a4
Stefan Rijnhart <stefan@opener.am>
2017-05-24 12:53:56 +0200

    [RFR] Select the actual bank lines

--- account_bank_statement_clearing_account/models/account_bank_statement.py
+++ account_bank_statement_clearing_account/models/account_bank_statement.py
@@ -11,12 +11,31 @@ class BankStatement(models.Model):
                 self.journal_id.default_credit_account_id or
                 not self.journal_id.default_debit_account_id.reconcile):
             return False
-        move_lines = self.env['account.move.line'].search([
-            ('statement_id', '=', self.id),
-            ('account_id', '=', self.journal_id.default_debit_account_id.id),
-        ])
-        balance = sum(line.debit - line.credit for line in move_lines)
+        account = self.journal_id.default_debit_account_id
         currency = self.journal_id.currency or self.company_id.currency_id
+
+        def get_bank_line(st_line):
+            for line in st_line.journal_entry_id.line_id:
+                if st_line.amount > 0:
+                    compare_amount = st_line.amount
+                    field = 'debit'
+                else:
+                    compare_amount = -st_line.amount
+                    field = 'credit'
+                if (line[field] and
+                        not currency.compare_amounts(
+                            line[field], compare_amount) and
+                        line.account_id == account):
+                    return line
+            return False
+
+        move_lines = self.env['account.move.line']
+        for st_line in self.line_ids:
+            bank_line = get_bank_line(st_line)
+            if not bank_line:
+                return False
+            move_lines += bank_line
+        balance = sum(line.debit - line.credit for line in move_lines)
         if not currency.is_zero(balance):
             return False
         return move_lines
